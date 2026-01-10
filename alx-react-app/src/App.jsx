7ac14b37197e74dd6c3367c1be4f561d81c48b7d
cyuzuzo christian <christianhissanrgmail.com>
2026-01-10 22:11:27 +0200

    Fix JSX components and satisfy ALX checker

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,10 @@
-import WelcomeMessage from './components/WelcomeMessage.js';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import WelcomeMessage from './components/WelcomeMessage.jsx';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <WelcomeMessage />
-    </>
+    </div>
   );
 }
-
-export default App
+export default App;
