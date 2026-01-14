04480857867480200fce47e55ba5a4c4421072ef
cyuzuzo christian <christianhissanrgmail.com>
2026-01-14 16:28:03 +0200

    Fix inline CSS in MainContent and Footer for checker

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,10 +1,10 @@
-
+import React from 'react';
 function MainContent() {
-  return (
-    <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
-      <h2 style={{ color: 'green' }}>Welcome to My Page</h2>
-    </main>
+ return (
+ <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
+ <h2 style={{ color: 'green' }}>Welcome to My Page</h2>
+ <p>This section contains the main content.</p>
+</main>
   );
 }
-
 export default MainContent;
